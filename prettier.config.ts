b425a9b2f6bdb3e245644fa7c3826ad4724d8bc2
climoe <klimczak.kamill@gmai.com>
2026-08-03 10:17:17 +0200

    Upgrade - Prettier configuration for project

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 1,
+  useTabs: true,
+  printWidth: 120
+};
+
+export default config;
